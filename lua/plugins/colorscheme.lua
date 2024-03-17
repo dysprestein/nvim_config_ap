08de4ee264e1f62ef5378f8236bc337111c816f9
dysprestein <amanpathak7@gmail.com>
2024-03-18 00:32:05 +0530

    feat: new colorschemes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  { "ellisonleao/gruvbox.nvim",
+    lazy = false,
+  },
+  { "sainnhe/gruvbox-material",
+    lazy = false,
+  },
+  { "catppuccin/nvim",
+    lazy = false,},
+  { "folke/tokyonight.nvim",
+    lazy = false,
+  },
+  { "rebelot/kanagawa.nvim",
+    lazy = false,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
